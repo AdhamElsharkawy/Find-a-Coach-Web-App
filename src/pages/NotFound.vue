54f4a8e00b197577167d4d5e7a5740fdffcebb72
adham <adham.elsharkawy@blueholding.co.uk>
2023-05-30 16:38:17 +0300

    DemoAuth

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+   <base-card>
+    <h2>Page Not Found</h2>
+    <p>Sorry, the page you are looking for does not exist- may be check out all our <router-link to="/coaches">coaches</router-link></p>
+   </base-card>
+</template>
